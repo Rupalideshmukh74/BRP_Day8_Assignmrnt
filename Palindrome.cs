fca44f7555fa724c2e65fc6be3a1a596dd0452fd
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-10-25 10:08:59 +0530

    [Rupali]Feat:Palindrome program added

---
+++ Palindrome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day8_Assignment
+{
+    class Palindrome
+    {
+        public void palindrome()
+        {
+             
+            int rem, rev=0,num=0; int temp = num;
+            Console.WriteLine("Enter the number");
+             num = int.Parse(Console.ReadLine());
+
+            while (temp != 0)
+            {
+                rem = num % 10;
+                rev = rev * 10 + rem;
+                num = temp/ 10;
+
+
+            }
+            if (temp == rev)
+            {
+                Console.WriteLine("Number is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Number is not Palindrome");
+            }
+        }
+        
+    }
+}
